43721f9303f79acff470e3d3f4b1116ceeb32921
sergi <sergi@Sergio>
2024-11-27 10:09:46 +0100

    Septima subida. Icono y nombre de la aplicación cambiados.

--- TFG/src/game/Launcher.java
+++ TFG/src/game/Launcher.java
@@ -2,12 +2,20 @@ package game;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-public class Launcher
-{
-    public static void main (String[] args)
-    {
-        Game myGame = new Demo(); 
-        LwjglApplication launcher = new LwjglApplication( myGame, "2021", Parametros.getAnchoPantalla(), Parametros.getAltoPantalla());
+public class Launcher {
+    public static void main(String[] args) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Enoden";
+        config.width = Parametros.getAnchoPantalla();
+        config.height = Parametros.getAltoPantalla();
+
+
+        config.addIcon("icons/logo32.png", com.badlogic.gdx.Files.FileType.Internal);
+      
+
+        Game myGame = new Demo();
+        new LwjglApplication(myGame, config);
     }
 }
